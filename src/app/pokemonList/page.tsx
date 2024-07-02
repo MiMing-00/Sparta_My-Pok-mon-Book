422d47070a978095a59c25308358df73355b4de3
Sera Choi <c.sera2511@gmail.com>
2024-07-02 20:04:50 +0900

    [add] : update api

---
+++ src/app/pokemonList/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Pokemon = {
+  id: number;
+  name: string;
+  korean_name: string;
+  height: number;
+  weight: number;
+  sprites: { front_default: string };
+  types: { type: { name: string; korean_name: string } }[];
+  abilities: { ability: { name: string; korean_name: string } }[];
+  moves: { move: { name: string; korean_name: string } }[];
+};
+
+const PokemonPage = () => {
+  return <div>PokemonPage</div>;
+};
+
+export default PokemonPage;
